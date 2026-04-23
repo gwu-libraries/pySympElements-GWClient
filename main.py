98bf67400b3d2510fb26499b92338fbb22ad742d
Dan Kerchner <kerchner@gwu.edu>
2026-04-23 10:36:04 -0400

    Add code to try getting a user's publications and their open access status

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ user = client.get_object('users', 767, detail='full')
 rel1 = client.get_relationships(object=f'user({user.id})')
 
 # Get all the publications with full details, limiting to 3 pages of results
-pubs = client.get_publications(detail = 'full', limit = 88)
+pubs = client.get_publications(detail = 'full', limit = 55)
 
 print(user.to_json())
 print(rel1) # Note that rel1 is a list of Relationship objects, not a JSON string. You can convert it to JSON if needed.
 print(pubs) # Note that pubs is a Python list of Publication objects, not a JSON string. You can convert it to JSON if needed.
+#for pub in pubs:
+#    print(pub.type_display_name, ": ", pub.open_access_status)
+
+pub_rels_list = client.get_related_objects(category_from = 'users', category_to = 'publications', id = 767, limit = 25, detail = 'full')
+
+for pub_rel in pub_rels_list:
+    pub = pub_rel.related[0].object
+    print(pub.fields_dict.get('open-access-status'))
